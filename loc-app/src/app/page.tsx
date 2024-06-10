28fd48701438534e119477d516eccba617f7cfa7
Florina <ffiador@gmail.com>
2024-06-10 12:52:00 +0100

    time page added

--- loc-app/src/app/page.tsx
+++ loc-app/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Homepage } from "@/pages/homepage";
+import { Time } from "@/pages/time";
 import Link from "next/link";
 import { Date } from "@/pages/date";
 
 export default function Home() {
   return (
   
-    <Date/>
+    <Time/>
   
   );
 }
